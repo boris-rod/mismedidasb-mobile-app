96dd80863a81f237fd898d6737d7195f64282ed0
enrique <enrique.anchia84@gmail.com>
2020-02-06 01:33:02 -0500

    - Creating empty main models

--- lib/res/values/text/strings_base.dart
+++ lib/res/values/text/strings_base.dart
@@ -3,8 +3,12 @@ import 'package:flutter/widgets.dart';
 //en
 abstract class StringsBase implements WidgetsLocalizations {
   String get appName;
+
   String get myMeasureHealth;
+
   String get myMeasureValues;
+
   String get myMeasureWellness;
+
   String get foodDishes;
 }
