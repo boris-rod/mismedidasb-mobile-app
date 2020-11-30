2131f7568623da4b83eb0b8e6feff472ddd172c3
enrique <enrique.anchia84@gmail.com>
2020-11-30 14:02:32 -0500

    - Upgrading to version: 0.5.9+47

--- lib/ui/plani_service/plani_service_page.dart
+++ lib/ui/plani_service/plani_service_page.dart
@@ -109,7 +109,7 @@ class _PlaniServiceState
                                         color: Colors.white,
                                       ),
                                       Icon(
-                                        Icons.verified_rounded,
+                                        Icons.ac_unit,
                                         size: 14,
                                         color: Colors.white,
                                       )
@@ -197,7 +197,7 @@ class _PlaniServiceState
                                         text: model.valueCoins.toString(),
                                       ),
                                       Icon(
-                                        Icons.verified_rounded,
+                                        Icons.ac_unit,
                                         size: 14,
                                       )
                                     ],
